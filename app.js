8cee23043b81decc268109f58eca6ed79e0ea2a1
RomanSanaye <sanaye143@gmail.com>
2026-05-01 20:43:30 +0100

    service worker added

--- app.js
+++ app.js
@@ -263,18 +263,18 @@ document.getElementById("adsGyro").innerText = "Gyro ADS: None";
 };
 
 // SERVICE WORKER REGISTRATION
-if ("serviceWorker" in navigator) {
-window.addEventListener("load", () => {
-navigator.serviceWorker
-.register("/service-worker.js")
-.then((reg) => {
-console.log("Service Worker registered:", reg.scope);
-})
-.catch((err) => {
-console.log("Service Worker failed:", err);
-});
-});
-}
+// if ("serviceWorker" in navigator) {
+// window.addEventListener("load", () => {
+// navigator.serviceWorker
+// .register("/service-worker.js")
+// .then((reg) => {
+// console.log("Service Worker registered:", reg.scope);
+// })
+// .catch((err) => {
+// console.log("Service Worker failed:", err);
+// });
+// });
+// }
 
 // =====================
 loadDevices();
